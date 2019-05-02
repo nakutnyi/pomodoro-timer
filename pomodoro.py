a6e043dc2ff699ce17692b25b4aa6b3448c8cfd6
Roman Nakutnyi <nakutnyi@gmail.com>
2019-05-02 10:05:42 +0300

    Fix variables names (PEP8: constants)

--- pomodoro.py
+++ pomodoro.py
@@ -9,17 +9,17 @@ My personal take on the most popular productivity tool out there.
 
 
 import tkinter as tk
-
+from tkinter import messagebox
 
 def count(timer):
     """
     Countdown
     """
     global IS_BREAK
-    global job
+    global JOB
     global SESS_COUNTER
 
-    ask = tkinter.messagebox.askquestion
+    ask = messagebox.askquestion
 
     if timer <= -1:
 
@@ -37,10 +37,10 @@ def count(timer):
 
 
         if prompt_answer == 'yes' and SESS_COUNTER % 4 != 0 and IS_BREAK:
-            root.after_cancel(job)
+            ROOT.after_cancel(JOB)
             count(SHORT_BREAK)
         elif prompt_answer == 'yes' and SESS_COUNTER % 4 == 0 and IS_BREAK:
-            root.after_cancel(job)
+            ROOT.after_cancel(JOB)
             count(LONG_BREAK)
         elif prompt_answer == 'no':
             stop_count()
@@ -49,13 +49,13 @@ def count(timer):
             count(SESSION)
         return
 
-    m, s = divmod(timer, 60)
-    time_label.configure(text='{:02d}:{:02d}'.format(m, s))
+    minutes, seconds = divmod(timer, 60)
+    TIME_LABEL.configure(text='{:02d}:{:02d}'.format(minutes, seconds))
     if IS_BREAK:
-        cnt_label.configure(text='BREAK!')
+        CNT_LABEL.configure(text='BREAK!')
     else:
-        cnt_label.configure(text='Streak: {}'.format(SESS_COUNTER))
-    job = root.after(1000, count, timer - 1)
+        CNT_LABEL.configure(text='Streak: {}'.format(SESS_COUNTER))
+    JOB = ROOT.after(1000, count, timer - 1)
 
 
 def stop_count():
@@ -65,20 +65,20 @@ def stop_count():
     global SESS_COUNTER
     global IS_BREAK
 
-    root.after_cancel(job)
-    time_label.configure(text='{:02d}:{:02d}'.format(0, 0))
+    ROOT.after_cancel(JOB)
+    TIME_LABEL.configure(text='{:02d}:{:02d}'.format(0, 0))
     SESS_COUNTER = 0
     IS_BREAK = False
-    cnt_label.configure(text='Streak: {}'.format(0))
-    start_btn.configure(text="Start", command=lambda: start())
+    CNT_LABEL.configure(text='Streak: {}'.format(0))
+    START_BTN.configure(text="Start", command=lambda: start())
 
 
 # pauses the counter
 def pause_count():
-    global time_label
+    global TIME_LABEL
 
-    start_btn.configure(text="Cont.", command=continue_count)
-    root.wait_window(time_label)
+    START_BTN.configure(text="Cont.", command=continue_count)
+    ROOT.wait_window(TIME_LABEL)
 
 
 # continue after pause
@@ -93,7 +93,7 @@ def start():
     global SESS_COUNTER
 
     SESS_COUNTER += 1
-    start_btn.configure(command=tk.DISABLED)
+    START_BTN.configure(command=tk.DISABLED)
     count(SESSION)
 
 # VARIABLE DECLARATIONS
@@ -111,48 +111,48 @@ IS_BREAK = False
 
 # TKINTER SETTINGS
 
-# root & title
-root = tk.Tk()
-root.title('Pomodoro')
-root.geometry('200x60')
+# ROOT & title
+ROOT = tk.Tk()
+ROOT.title('Pomodoro')
+ROOT.geometry('200x60')
 
 
 # labels
 # main label area
-main_label = tk.Frame(root)
-main_label.grid(row=2, column=3, sticky='nesw')
+MAIN_LABEL = tk.Frame(ROOT)
+MAIN_LABEL.grid(row=2, column=3, sticky='nesw')
 
 # column padding in window
-root.grid_columnconfigure(1, weight=1)
-root.grid_columnconfigure(2, weight=1)
-root.grid_columnconfigure(3, weight=1)
+ROOT.grid_columnconfigure(1, weight=1)
+ROOT.grid_columnconfigure(2, weight=1)
+ROOT.grid_columnconfigure(3, weight=1)
 
 # row padding in window
-root.grid_rowconfigure(1, weight=1)
-root.grid_rowconfigure(2, weight=1)
+ROOT.grid_rowconfigure(1, weight=1)
+ROOT.grid_rowconfigure(2, weight=1)
 
 
 # time label
-time_label = tk.Label(main_label, text='00:00')
-time_label.grid(row=1, column=1, columnspan=1)
+TIME_LABEL = tk.Label(MAIN_LABEL, text='00:00')
+TIME_LABEL.grid(row=1, column=1, columnspan=1)
 
 # placehodler label
-placeholder_label = tk.Label(main_label, text=' ~ ')
-placeholder_label.grid(row=1, column=2)
+PLACEHOLDER_LABEL = tk.Label(MAIN_LABEL, text=' ~ ')
+PLACEHOLDER_LABEL.grid(row=1, column=2)
 
 # counter label
-cnt_label = tk.Label(main_label, text='Streak: 0')
-cnt_label.grid(row=1, column=3, columnspan=1)
+CNT_LABEL = tk.Label(MAIN_LABEL, text='Streak: 0')
+CNT_LABEL.grid(row=1, column=3, columnspan=1)
 
 
 # buttons
-start_btn = tk.Button(main_label, text="Start", command=start)
-start_btn.grid(row=2, column=1)
-pause_btn = tk.Button(main_label, text="Pause", command=pause_count)
-pause_btn.grid(row=2, column=2)
-stop_btn = tk.Button(main_label, text="Stop", command=stop_count)
-stop_btn.grid(row=2, column=3)
+START_BTN = tk.Button(MAIN_LABEL, text="Start", command=start)
+START_BTN.grid(row=2, column=1)
+PAUSE_BTN = tk.Button(MAIN_LABEL, text="Pause", command=pause_count)
+PAUSE_BTN.grid(row=2, column=2)
+STOP_BTN = tk.Button(MAIN_LABEL, text="Stop", command=stop_count)
+STOP_BTN.grid(row=2, column=3)
 
 
 # MAINLOOP
-root.mainloop()
+ROOT.mainloop()
